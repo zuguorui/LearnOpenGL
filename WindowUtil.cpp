03514af4e5862740e9ca7f6b40203ed5d1aa663c
zu <zu@zudeMacBook-Pro.local>
2020-08-25 18:58:33 +0800

    merge

--- WindowUtil.cpp
+++ WindowUtil.cpp
@@ -13,11 +13,6 @@ void frameBufferSizeCallback(GLFWwindow *window, int width, int height)
     glViewport(0, 0, width, height);
 }
 
-void frameBufferSizeCallback(GLFWwindow *window, int width, int height)
-{
-    glViewport(0, 0, width, height);
-}
-
 void key_callback(GLFWwindow *window, int key, int scancode, int action, int mode)
 {
     if(key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
